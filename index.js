9f5224c9f3c4cd4c29c6fd01695792a4e025c12c
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-07-12 16:52:41 -0700

    Added prompt and file generation logic

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const SVG = require('./lib/svg')
+const inquirer = require('inquirer')
 
-const svg = new SVG()
+const questions = [{
+    type: 'input',
+    name: 'logoText',
+    message: 'Enter upto 3 character for your logo text?',
+    required: true
+  },{
+    type: 'input',
+    name: 'logoTextColor',
+    message: 'Enter logo text color as color keyword or a hexadecimal number',
+    required: true
+  },{
+    type: 'list',
+    message: 'Pick a shape for your logo',
+    name: 'shape',
+    choices: [ 'Triangle','Square','Circle']
+  },{
+    type: 'input',
+    name: 'logoColor',
+    message: 'Enter logo color as color keyword or a hexadecimal number',
+    required: true
+  },
+]
 
-svg.setShape('Square','blue')
-svg.setText('IND','yellow')
+inquirer.prompt(questions).then((data)=>{
+    //console.log(data)
+    const svg = new SVG()
 
-svg.createLogo()
+    svg.setShape(data.shape,data.logoColor)
+    svg.setText(data.logoText,data.logoTextColor)
+
+    svg.createLogo()
+})
+
+
+
