74f5f7748de316f969948b482f4e3e6786cf2781
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-07-12 15:08:14 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 10-OOP-LOGO-Generator
 Application to generate the logo based on user inputs
+
+![](./examples/sample.svg)
---
+++ index.js
@@ -0,0 +1,8 @@
+const SVG = require('./lib/svg')
+
+const svg = new SVG()
+
+svg.setShape('Square','blue')
+svg.setText('IND','yellow')
+
+svg.createLogo()
